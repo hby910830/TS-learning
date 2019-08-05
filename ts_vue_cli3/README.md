483c7ab407f7e5ce7ac2f6b851f225622072290e
hanbaoyi <604594231@qq.com>
2019-08-05 20:36:43 +0800

    ts_vue_cli3

---
+++ ts_vue_cli3/README.md
@@ -0,0 +1,16 @@
+# ts_vue_cli3
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
---
+++ ts_vue_cli3/src/components/Hanbaoyi.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    i am hanbaoyi
+    <hello-world/>
+  </div>
+
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import HelloWorld from './HelloWorld.vue'
+
+    @Component({
+        components: {HelloWorld}
+    })
+    export default class Hanbaoyi extends Vue {
+    }
+</script>
