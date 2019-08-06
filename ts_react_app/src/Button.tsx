130263eb6ecf7a70b6b1c0a847fdc2dfe603fa1f
hanbaoyi <604594231@qq.com>
2019-08-07 01:08:54 +0800

    Button接受children属性类型

--- ts_react_app/src/Button.tsx
+++ ts_react_app/src/Button.tsx
@@ -7,6 +7,9 @@ interface IProps {
 
 // type Props = {size: string}  也可以这么声明类型
 
-export default function Button(props:IProps){
-  return <div className={`button ${props.size}`}>button</div>
+export default function Button(props: IProps) {
+  return <div className={`button ${props.size}`}>
+    {/*这里的children是父元素的【点我】文字*/}
+    {props.children}
+  </div>
 }
--- ts_react_app/src/index.tsx
+++ ts_react_app/src/index.tsx
@@ -5,9 +5,9 @@ import Button from './Button'
 
 ReactDOM.render(
   <div>
-            <Button size="small"></Button>
-            <Button></Button>
-            <Button size="big"></Button>
+            <Button size="small"><span>点我</span></Button>
+            <Button>点我</Button>
+            <Button size="big">点我</Button>
           </div>,
   document.getElementById('root')
 );
