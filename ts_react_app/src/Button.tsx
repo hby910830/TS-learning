5dd33fed20928be2127eda2717dd5ba97c0a39bf
hanbaoyi <604594231@qq.com>
2019-08-07 01:15:23 +0800

    props的children接受JSX.Element[]类型

--- ts_react_app/src/Button.tsx
+++ ts_react_app/src/Button.tsx
@@ -4,7 +4,8 @@ import './Button.css'
 interface IProps {
   size?: string;
   //JSX.Element是JSX的element类型，对应父元素的span标签
-  children?: string | JSX.Element  /*当有子元素的时候，react规定子组件必须有children属性。这里的children是父元素的【点我】文字*/
+  // JSX.Element[]是指接受多个子元素标签为数组的类型，对应父元素的两个span标签
+  children?: string | JSX.Element | JSX.Element[]  /*当有子元素的时候，react规定子组件必须有children属性。这里的children是父元素的【点我】文字*/
 }
 
 // type Props = {size: string}  也可以这么声明类型
--- ts_react_app/src/index.tsx
+++ ts_react_app/src/index.tsx
@@ -5,7 +5,10 @@ import Button from './Button'
 
 ReactDOM.render(
   <div>
-            <Button size="small"><span>点我</span></Button>
+            <Button size="small">
+              <span>点我</span>
+              <span>点我</span>
+            </Button>
             <Button>点我</Button>
             <Button size="big">点我</Button>
           </div>,
