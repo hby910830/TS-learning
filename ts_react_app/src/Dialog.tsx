8293d97b9933cdab2ef8fc608fccae6048bdeb6f
hanbaoyi <604594231@qq.com>
2019-08-07 01:52:28 +0800

    React.MouseEvent.MouseEventHandler鼠标事件类型

--- ts_react_app/src/Dialog.tsx
+++ ts_react_app/src/Dialog.tsx
@@ -6,7 +6,7 @@ interface IProps {
   onClick?: React.MouseEventHandler;  //react的鼠标事件处理器类型
 }
 
-{/*<IProps>是react的props参数*/}
+/*<IProps>是react的props参数*/
 const Dialog:React.FunctionComponent<IProps>= function(props){
   return <div className={`button ${props.size}`} onClick={props.onClick}>{props.children}</div>
 }
