8db94b1e44c1120f4b914c81dbfd58918d0d98ff
hanbaoyi <604594231@qq.com>
2019-08-08 19:34:16 +0800

    Partial<Person>，Required<Person>

--- High-level-types/Readonly-Partial.js
+++ High-level-types/Readonly-Partial.js
@@ -1,20 +1,39 @@
-var person = {
-    name: 'hby',
-    age: 18
-};
-person.name = 'jack';
-var person1 = {
-    name: 'hby',
-    age: 18
-};
-var personReadonly = {
-    name: 'hby',
-    age: 18
-};
-//personReadonly.name = 'tom' //报错,Cannot assign to 'name' because it is a constant or a read-only property.
-//可以简写：
-var personReadonly1 = {
-    name: 'hby',
-    age: 18
-};
-//personReadonly1.age = 12  //报错， Cannot assign to 'age' because it is a constant or a read-only property.
+{
+    var person = {
+        name: 'hby',
+        age: 18
+    };
+    person.name = 'jack';
+    var person1 = {
+        name: 'hby',
+        age: 18
+    };
+    var personReadonly = {
+        name: 'hby',
+        age: 18
+    };
+    //personReadonly.name = 'tom' //报错,Cannot assign to 'name' because it is a constant or a read-only property.
+    //可以简写：
+    var personReadonly1 = {
+        name: 'hby',
+        age: 18
+    };
+    //personReadonly1.age = 12  //报错， Cannot assign to 'age' because it is a constant or a read-only property.
+}
+{
+    var person = {
+        name: 'hby'
+    };
+    // 报错，Type '{ name: string; }' is not assignable to type 'Person'.Property 'age' is missing in type '{ name: string; }'.
+    // const person1: Person = {
+    //   name: 'hby'
+    // }
+    var person2 = {
+        name: 'hby'
+    };
+    //Required和Partials是两个相反的操作
+    // 报错，Type '{ name: string; }' is not assignable to type 'Required<Person>'. Property 'age' is missing in type '{ name: string; }'.
+    var person3 = {
+        name: 'hby'
+    };
+}
--- High-level-types/Readonly-Partial.ts
+++ High-level-types/Readonly-Partial.ts
@@ -1,41 +1,73 @@
-interface Person {
-  name: string;
-  age: number
-}
+{
+  interface Person {
+    name: string;
+    age: number
+  }
 
-interface ReadOnlyPerson {
-  readonly name: string;
-  readonly age: number
-}
+  interface ReadOnlyPerson {
+    readonly name: string;
+    readonly age: number
+  }
 
-const person: Person = {
-  name: 'hby',
-  age: 18
-}
+  const person: Person = {
+    name: 'hby',
+    age: 18
+  }
 
-person.name = 'jack'
+  person.name = 'jack'
 
-const person1: ReadOnlyPerson = {
-  name: 'hby',
-  age: 18
-}
+  const person1: ReadOnlyPerson = {
+    name: 'hby',
+    age: 18
+  }
 
 //person1.name = 'jack'  //报错，Cannot assign to 'name' because it is a constant or a read-only property
 
 
-type ReadonlyPerson2 = Readonly<Person>
-const personReadonly: ReadonlyPerson2 = {
-  name: 'hby',
-  age: 18
-}
+  type ReadonlyPerson2 = Readonly<Person>
+  const personReadonly: ReadonlyPerson2 = {
+    name: 'hby',
+    age: 18
+  }
 //personReadonly.name = 'tom' //报错,Cannot assign to 'name' because it is a constant or a read-only property.
 
 
 //可以简写：
-const personReadonly1: Readonly<Person> = {
-  name: 'hby',
-  age: 18
-}
+  const personReadonly1: Readonly<Person> = {
+    name: 'hby',
+    age: 18
+  }
 //personReadonly1.age = 12  //报错， Cannot assign to 'age' because it is a constant or a read-only property.
+}
 
+
+{
+  interface Person{
+    name: string;
+    age:number;
+    gender: string
+  }
+  interface Person1{
+    name?: string;
+    age?:number;
+    gender?: string
+  }
+  const person: Person1 = {
+    name: 'hby'
+  }
+  // 报错，Type '{ name: string; }' is not assignable to type 'Person'.Property 'age' is missing in type '{ name: string; }'.
+  // const person1: Person = {
+  //   name: 'hby'
+  // }
+  const person2: Partial<Person> = {
+    name: 'hby'
+  }
+
+  //Required和Partials是两个相反的操作
+
+  // 报错，Type '{ name: string; }' is not assignable to type 'Required<Person>'. Property 'age' is missing in type '{ name: string; }'.
+  const person3: Required<Person> = {
+    name: 'hby'
+  }
+}
 
