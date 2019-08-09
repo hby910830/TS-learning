70e52c898b2c42a1af429d33ea3e855a2c0fda1b
hanbaoyi <604594231@qq.com>
2019-08-09 23:24:16 +0800

    declare | export default | export {X} 的区别

--- declaration_files/a.d.ts
+++ declaration_files/a.d.ts
@@ -1,3 +1,7 @@
 declare function y(a: number, b: number): number; //declare也可以写成export
-export function add2(a: number): string
+
+// export function add2(a: number): string  // 第一种写法
+declare function add2(a: number): string    // 第二种写法
+export {add2}
+
 export default y
