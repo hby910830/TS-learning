97afe1881dc4280236e21135452bd388017127eb
hanbaoyi <604594231@qq.com>
2019-08-09 21:51:27 +0800

    Redux的Action可识别联合类型

--- High-level-types/discriminated-unions.js
+++ High-level-types/discriminated-unions.js
@@ -11,3 +11,19 @@ function fn(a) {
 }
 fn({ action: 'update', id: 1, name: 'jack' });
 fn({ action: 'create', name: 'hby' });
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD':
+            action.payload; //number
+            break;
+        case 'ADD_STRING':
+            action.payload; //string
+            break;
+        case 'ADD_DATE':
+            action.payload; //Date
+            break;
+        case 'ADD_USER':
+            action.payload; //User
+            break;
+    }
+}
--- High-level-types/discriminated-unions.ts
+++ High-level-types/discriminated-unions.ts
@@ -23,4 +23,45 @@ fn({action: 'update', id: 1, name: 'jack'})
 fn({action: 'create', name: 'hby'})
 
 
+type Action = {
+  type: string;
+  payload: any
+}
+
+interface User {
+  id: number;
+  name: string
+}
+
+type Action2 = {
+  type: 'ADD';
+  payload: number
+} | {
+  type: 'ADD_STRING';
+  payload: string
+} | {
+  type: 'ADD_DATE';
+  payload: Date
+} | {
+  type: 'ADD_USER';
+  payload: User
+}
+
+function reducer(state:any,action: Action2){
+  switch (action.type) {
+    case 'ADD':
+      action.payload    //number
+      break;
+    case 'ADD_STRING':
+      action.payload    //string
+      break;
+    case 'ADD_DATE':
+      action.payload    //Date
+      break;
+    case 'ADD_USER':
+      action.payload    //User
+      break;
+  }
+}
+
 
